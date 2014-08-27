b572a13f77db0acc73549bf3b02a944aae562f6e
audioniek <ndevries@xs4all.nl>
2014-08-27 15:05:07 +0200

    add fortis hs7119 and hs7819

--- tools/fp_control/fp_control.c
+++ tools/fp_control/fp_control.c
@@ -739,9 +739,9 @@ int getModel()
 			vBoxType = Fortis;
 		else if (!strncasecmp(vName, "hs7110", 6))
 			vBoxType = Fortis;
-		else if (!strncasecmp(vName, "hs7119", 6))
-			vBoxType = Fortis;
 		else if (!strncasecmp(vName, "hs7810a", 7))
+			vBoxType = Fortis;
+		else if (!strncasecmp(vName, "hs7119", 6))
 			vBoxType = Fortis;
 		else if (!strncasecmp(vName, "hs7819", 6))
 			vBoxType = Fortis;
