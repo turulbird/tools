231f329883552b180853af036671775c6f0175b8
Audioniek <ndevries@xs4all.nl>
2020-02-25 17:37:26 +0100

    fp_control: add Fortis 4G models

--- tools/fp_control/fp_control.c
+++ tools/fp_control/fp_control.c
@@ -35,7 +35,7 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /* Software version of fp_control, please increase on every change */
-static const char *sw_version = "1.11Audioniek 20190814.1";
+static const char *sw_version = "1.11Audioniek 20200225.1";
 static eWakeupReason reason = 0;
 
 typedef struct
@@ -777,56 +777,60 @@ int getModel()
 		{
 			vBoxType = Vip2;
 		}
-		else
-			if ((!strncasecmp(vName, "hdbox", 5))
-			|| (!strncasecmp(vName, "octagon1008", 11))
-			|| (!strncasecmp(vName, "atevio7500", 10))
-			|| (!strncasecmp(vName, "hs7110", 6))
-			|| (!strncasecmp(vName, "hs7420", 6))
-			|| (!strncasecmp(vName, "hs7810a", 7))
-			|| (!strncasecmp(vName, "hs7119", 6))
-			|| (!strncasecmp(vName, "hs7429", 6))
-			|| (!strncasecmp(vName, "hs7819", 6)))
-			{
-				vBoxType = Fortis;
-			}
-		else
-			if ((!strncasecmp(vName, "atemio520", 9))
-			|| (!strncasecmp(vName, "atemio530", 9)))
-			{
-				vBoxType = CNBox;
-			}
+		else if ((!strncasecmp(vName, "hdbox", 5))
+		      || (!strncasecmp(vName, "octagon1008", 11))
+		      || (!strncasecmp(vName, "atevio7500", 10))
+		      || (!strncasecmp(vName, "hs7110", 6))
+		      || (!strncasecmp(vName, "hs7420", 6))
+		      || (!strncasecmp(vName, "hs7810a", 7))
+		      || (!strncasecmp(vName, "hs7119", 6))
+		      || (!strncasecmp(vName, "hs7429", 6))
+		      || (!strncasecmp(vName, "hs7819", 6))
+		      || (!strncasecmp(vName, "dp2010", 6))
+		      || (!strncasecmp(vName, "dp6010", 6))
+		      || (!strncasecmp(vName, "dp7000", 6))
+		      || (!strncasecmp(vName, "dp7001", 6))
+		      || (!strncasecmp(vName, "dp7050", 6))
+		      || (!strncasecmp(vName, "ep8000", 6))
+		      || (!strncasecmp(vName, "epp8000", 7))
+		      || (!strncasecmp(vName, "fx6010", 6))
+		      || (!strncasecmp(vName, "gpv8000", 7)))
+		{
+			vBoxType = Fortis;
+		}
+		else if ((!strncasecmp(vName, "atemio520", 9))
+		      || (!strncasecmp(vName, "atemio530", 9)))
+		{
+			vBoxType = CNBox;
+		}
 		else if (!strncasecmp(vName, "hs5101", 6))
 		{
 			vBoxType = Hs5101;
 		}
-		else
-			if ((!strncasecmp(vName, "spark", 5))
-			|| (!strncasecmp(vName, "spark7162", 9)))
-			{
-				vBoxType = Spark;
-			}
+		else if ((!strncasecmp(vName, "spark", 5))
+		      || (!strncasecmp(vName, "spark7162", 9)))
+		{
+			vBoxType = Spark;
+		}
 		else if (!strncasecmp(vName, "adb_box", 7))
 		{
 			vBoxType = Adb_Box;
 		}
-		else
-			if ((!strncasecmp(vName, "cuberevo", 8))
-		    || (!strncasecmp(vName, "cuberevo-mini", 13))
-		    || (!strncasecmp(vName, "cuberevo-mini2", 14))
-		    || (!strncasecmp(vName, "cuberevo-mini-fta", 17))
-		    || (!strncasecmp(vName, "cuberevo-250hd", 14))
-		    || (!strncasecmp(vName, "cuberevo-2000hd", 15))
-		    || (!strncasecmp(vName, "cuberevo-9500hd", 15))
-		    || (!strncasecmp(vName, "cuberevo-3000hd", 14)))
-			{
-				vBoxType = Cuberevo;
-			}
-		else
-			if (!strncasecmp(vName, "vitamin_hd5000", 14))
-			{
-				vBoxType = Vitamin_HD5000;
-			}
+		else if ((!strncasecmp(vName, "cuberevo", 8))
+		      || (!strncasecmp(vName, "cuberevo-mini", 13))
+		      || (!strncasecmp(vName, "cuberevo-mini2", 14))
+		      || (!strncasecmp(vName, "cuberevo-mini-fta", 17))
+		      || (!strncasecmp(vName, "cuberevo-250hd", 14))
+		      || (!strncasecmp(vName, "cuberevo-2000hd", 15))
+		      || (!strncasecmp(vName, "cuberevo-9500hd", 15))
+		      || (!strncasecmp(vName, "cuberevo-3000hd", 14)))
+		{
+			vBoxType = Cuberevo;
+		}
+		else if (!strncasecmp(vName, "vitamin_hd5000", 14))
+		{
+			vBoxType = Vitamin_HD5000;
+		}
 		else
 		{
 			vBoxType = Unknown;
