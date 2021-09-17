4a803d5f018d3b6791fdd98133e8289dbd2e2381
audioniek <ndevries@xs4all.nl>
2021-09-17 19:50:34 +0200

    evremote 2 cnbox: add missing atemio520 RC keys

--- evremote2/Cnbox.c
+++ evremote2/Cnbox.c
@@ -58,10 +58,10 @@ static tButton cButtonCnbox[] =  // order is same as on HD 9600 RC
 {
 	{ "POWER",       "5F", KEY_POWER },
 	{ "MUTE",        "57", KEY_MUTE },
-	{ "HELP",        "11", KEY_HELP },
-	{ "TV_FORMAT",   "07", KEY_ZOOM },
+	{ "HELP",        "11", KEY_HELP },  // Labeled MEDIA on Atemio520
+	{ "TV_FORMAT",   "07", KEY_ZOOM },  // Labeled RES on Atemio520
 	{ "TVRADIO",     "14", KEY_TV2 },
-	{ "MODE",        "1a", KEY_SWITCHVIDEOMODE },
+	{ "MODE",        "1a", KEY_SWITCHVIDEOMODE },  // Labeled PIP on Atemio520
 	{ "1",           "56", KEY_1 },
 	{ "2",           "5A", KEY_2 },
 	{ "3",           "5E", KEY_3 },
@@ -73,10 +73,10 @@ static tButton cButtonCnbox[] =  // order is same as on HD 9600 RC
 	{ "9",           "5C", KEY_9 },
 	{ "MENU",        "4A", KEY_MENU },
 	{ "0",           "1B", KEY_0 },
-	{ "RECALL",      "15", KEY_BACK },
+	{ "RECALL",      "15", KEY_BACK },  // Labeled BACK on Atemio520
 	{ "REWIND",      "1E", KEY_REWIND },
 	{ "STOP",        "10", KEY_STOP },
-	{ "PLAYPAUSE",   "12", KEY_PLAYPAUSE },
+	{ "PLAYPAUSE",   "12", KEY_PLAYPAUSE },  // Labeled PLAY on Atemio520
 	{ "FASTFORWARD", "01", KEY_FASTFORWARD },
 	{ "RECORD",      "1C", KEY_RECORD },
 	{ "UP",          "18", KEY_UP },
@@ -96,26 +96,24 @@ static tButton cButtonCnbox[] =  // order is same as on HD 9600 RC
 	{ "YELLOW",      "08", KEY_YELLOW },
 	{ "BLUE",        "03", KEY_BLUE },
 	{ "FAV",         "00", KEY_FAVORITES },
-	{ "STATUS",      "0C", KEY_AUX },
-	{ "OPTION",      "19", KEY_OPTION },
-	{ "FIND",        "16", KEY_PROGRAM },
-
+	{ "STATUS",      "0C", KEY_HELP },  // Labelled HELP on Atemio520
+	{ "OPTION",      "19", KEY_TEXT },  // Labeled TEXT on Atemio520
+	{ "FIND",        "16", KEY_SUBTITLE },  // Labeled SUBTITLE on Atemio520
+// Keys present only on the Atemio 520 remote
+	{ "VOLUMEUP",    "22", KEY_VOLUMEUP },
+	{ "VOLUMEDOWN",  "23", KEY_VOLUMEDOWN },
+	{ "CHANNELUP",   "20", KEY_CHANNELUP },
+	{ "CHANNELDOWN", "21", KEY_CHANNELDOWN },
+	{ "PLUGIN",      "60", KEY_P },
+	{ "PAUSE",       "13", KEY_PAUSE },
+	{ "AUDIO",       "61", KEY_AUDIO },
+	{ "SHOOT",       "62", KEY_S },
+	{ "WWW",         "63", KEY_WWW },
+	{ "SLEEP",       "24", KEY_PROGRAM },
 #if 0
-	/* The following assignments were there in the original code
-	 * but make no sense. Some seem to have Fortis origins.
-	 */
-	{ "BACK",        "15", KEY_HOME },         // double for RECALL
-	{ "TEXT",        "03", KEY_TEXT },         // double for BLUE
-	{ "PAUSE",       "",   KEY_PAUSE },        // does not exist
-	{ "CHANNELUP",   "20", KEY_CHANNELUP },    // does not exist (AM530?)
-	{ "CHANNELDOWN", "21", KEY_CHANNELDOWN },  // does not exist (AM530?)
-	{ "VOLUMEUP",    "22", KEY_VOLUMEUP },     // does not exist (AM530?)
-	{ "VOLUMEDOWN",  "23", KEY_VOLUMEDOWN },   // does not exist (AM530?)
-	{ "RESOLUTION",  "07", KEY_SCREEN },       // double for TVFORMAT
-	{ "CHECK",       "42", KEY_SELECT },       // double for NEXT
-	{ "UPUP",        "43", KEY_PAGEUP },       // does not exist (AM530?)
-	{ "DOWNDOWN",    "44", KEY_PAGEDOWN },     // does not exist (AM530?)
-	{ "SLEEP",       "24", KEY_PROGRAM },      // does not exist (AM530?)
+	{ "CHECK",       "42", KEY_SELECT },  // double for NEXT
+	{ "UPUP",        "43", KEY_PAGEUP },  // does not exist
+	{ "DOWNDOWN",    "44", KEY_PAGEDOWN },  // does not exist
 #endif
 	{ "",            "",   KEY_NULL }
 };
@@ -220,7 +218,7 @@ static int pRead(Context_t *context)
 
 static int pNotification(Context_t *context, const int cOn)
 {
-	/* noop: there no controllable LEDs or icons on this model */
+	/* noop: there are no controllable LEDs or icons on this model */
 	return 0;
 }
 
